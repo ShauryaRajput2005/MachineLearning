0ac702aa4f28d4685414f088d19904ee1e2274df
Shikhar Shaurya <142671302+ShauryaRajput2005@users.noreply.github.com>
2024-11-09 19:11:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,13 @@ with st.expander('Data'):
   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
 
-st.write('**X**')
-X=df.drop('species',axis=1)
-X
-st.write('**Y**')
-y=df.species
-y
+  st.write('**X**')
+  X=df.drop('species',axis=1)
+  X
+  st.write('**Y**')
+  y=df.species
+  y
 
+with st.expander('Data Visualiation'):
+  #"bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g"
+  st.scatter(data=df,x='bill_length_mm',y='body_mass_g',color='species')
